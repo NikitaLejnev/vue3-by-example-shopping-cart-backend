ad874ad96737026bddee1a92667dbe5e28dd78d9
NikitaLejnev <catassasin331@gmail.com>
2022-01-28 09:19:18 +0300

    Added the middleware to app.js

--- app.js
+++ app.js
@@ -96,3 +96,16 @@ const authMiddleware = (req, res, next) => {
   }
   next();
 }
+
+const app = express();
+app.use(cors())
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(authMiddleware)
+app.use('/graphql', graphqlHTTP({
+  schema,
+  rootValue: root,
+  graphiql: true,
+}));

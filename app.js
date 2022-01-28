565310e16cc5509f71404b568c0f331df113fff8
NikitaLejnev <catassasin331@gmail.com>
2022-01-28 08:59:40 +0300

    Added a schema to app.js

--- app.js
+++ app.js
@@ -10,3 +10,64 @@ const shopItemResolvers = require('./resolvers/shopItems')
 const orderResolvers = require('./resolvers/orders')
 const authResolvers = require('./resolvers/auth')
 const jwt = require('jsonwebtoken');
+
+const schema = buildSchema(`
+  type Response {
+    status: String
+  }
+
+  type Token {
+    token: String
+  }
+
+  input User {
+    username: String
+    password: String
+    token: String
+  }
+
+  input ShopItem {
+    shop_item_id: Int
+    name: String
+    description: String
+    image_url: String
+    price: Float
+  }
+
+  type ShopItemOutput {
+    shop_item_id: Int
+    name: String
+    description: String
+    image_url: String
+    price: Float
+  }
+
+  type OrderOutput {
+    order_id: Int
+    name: String
+    address: String
+    phone: String
+    ordered_items: [ShopItemOutput]
+  }
+
+  input Order {
+    order_id: Int
+    name: String
+    address: String
+    phone: String
+    ordered_items: [ShopItem]
+  }
+
+  type Query {
+    getShopItems: [ShopItemOutput],
+    getOrders: [OrderOutput]
+  }
+
+  type Mutation {
+    addShopItem(shopItem: ShopItem): Response
+    removeShopItem(shopItemId: Int): Response
+    addOrder(order: Order): Response
+    removeOrder(orderId: Int): Response
+    login(user: User): Token
+  }
+`);
